bf388b0fba4bd489f278a12999b81fed544dba9c
David Beaudette <beaudette.david@gmail.com>
2018-04-29 10:08:18 -0600

    Fixed live Watson queries parsing.

--- watson/show_image.py
+++ watson/show_image.py
@@ -1,18 +1,21 @@
 from __future__ import division
 import cv2 as cv
 
-def show_image(filepath, situation, face_type, face_pos):
+def show_image(filename, filepath, situation, face_type, face_pos):
     img = cv.imread(filepath)
     
     font = cv.FONT_HERSHEY_SIMPLEX
     if situation:
-        cv.putText(img, situation[0], (10, 120), font, 2, (0,0,255), 3)
-    text_ver_pos = 220;
+        cv.putText(img, situation[0], (20, 1000), font, 2, (0,0,255), 3)
+    text_ver_pos = 1200;
     for ele in face_type:
-        cv.putText(img, ele, (10, text_ver_pos), font, 2, (0,0,0), 3)
+        cv.putText(img, ele, (20, text_ver_pos), font, 2, (0,0,0), 3)
         text_ver_pos = text_ver_pos + 100
     for pos in face_pos:
         cv.rectangle(img, (pos[0], pos[1]), (pos[0] + pos[2], pos[1] + pos[3]), (0,0,0), 3)
+
+    cv.imwrite(filename.replace('jpg','bmp'), img)
+    
     screen_res = 1280, 720
     scale_width = screen_res[0] / img.shape[1]
     scale_height = screen_res[1] / img.shape[0]
